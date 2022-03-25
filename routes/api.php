b1c1def34a8d34a4acfde8f7a31b3c5ed6f7d606
tanaboot <what_is_it_slow_k@hotmail.com>
2022-03-26 00:39:25 +0700

    two

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Api
+Route::post('/login',[\App\Http\Controllers\AuthController::class,'login']);
+Route::post('/logout',[\App\Http\Controllers\AuthController::class,'logout']);
+Route::post('/register',[\App\Http\Controllers\AuthController::class,'register']);
+//Public
+Route::get('/product',[\App\Http\Controllers\ProductController::class,'index']);
+Route::get('/product/show/{id}',[\App\Http\Controllers\ProductController::class,'show']);
+Route::get('/product/search/{name}',[\App\Http\Controllers\ProductController::class,'search']);
+//Protect
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::post('/product/store',[\App\Http\Controllers\ProductController::class,'store']);
+    Route::post('/product/update',[\App\Http\Controllers\ProductController::class,'update']);
+    Route::delete('/product/{id',[\App\Http\Controllers\ProductController::class,'delete']);
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
